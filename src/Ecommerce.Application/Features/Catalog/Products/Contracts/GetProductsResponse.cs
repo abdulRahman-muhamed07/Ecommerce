c6696f2cf28449610fd7b8d4d335aa43cddf165f
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-14 19:38:05 +0300

    feat: add product repsonse

---
+++ src/Ecommerce.Application/Features/Catalog/Products/Contracts/GetProductsResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Application.Features.Catalog.Products.Contracts
+{
+    public sealed record GetProductsResponse(
+    Guid Id,
+    string Name,
+    string Slug,
+    string? Description,
+    Guid BrandId,
+    Guid CategoryId
+);
+}
