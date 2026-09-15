05b17c31def50f60113f3310d9623e575a37ecee
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-15 15:55:54 +0300

    feat: register application services

--- src/Ecommerce.Api/Program.cs
+++ src/Ecommerce.Api/Program.cs
@@ -1,9 +1,11 @@
+using Ecommerce.Application;
 using Ecommerce.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
