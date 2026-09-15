08b01ac195878057632a96a554c710b1954f0d32
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-15 15:56:14 +0300

    fix: keep application validator registration framework independent

--- src/Ecommerce.Application/DependencyInjection.cs
+++ src/Ecommerce.Application/DependencyInjection.cs
@@ -1,5 +1,4 @@
 using FluentValidation;
-using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ecommerce.Application;
@@ -8,7 +7,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddFluentValidationAutoValidation();
         services.AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
 
         return services;
