912e26c1c77e549a58990a9bc2fe8bae2e272269
abdulRahman-muhamed07 <abdelrahmanelsepaay07@gmail.com>
2026-09-22 14:40:06 +0300

    fixing: CartItemConfiguration

---
+++ src/Ecommerce.Infrastructure/Persistence/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Ecommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ecommerce.Infrastructure.Persistence.Configurations;
+
+public sealed class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Quantity)
+            .IsRequired();
+
+        builder.Property(x => x.UnitPrice)
+            .HasPrecision(18, 2);
+
+        builder.HasIndex(x => new
+        {
+            x.CartId,
+            x.ProductVariantId
+        })
+        .IsUnique();
+
+        builder.HasOne(x => x.ProductVariant)
+            .WithMany()
+            .HasForeignKey(x => x.ProductVariantId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
